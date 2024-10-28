05a87e12da862461f480a91f8509a1c38b23bb9c
sajjad0057 <sajjadhossain0057@gmail.com>
2024-10-29 02:20:24 +0600

    practice handle client side error handling

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 export default function About() {
+    throw new Error("Fetal error occured!");
   return (
     <main className="mt-10">
         <div>This is about page!</div>
