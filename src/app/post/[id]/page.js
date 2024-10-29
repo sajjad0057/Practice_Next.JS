cd307f2e2deef71e026502a3fa3d556571888813
sajjad0057 <sajjadhossain0057@gmail.com>
2024-10-29 15:36:49 +0600

    make dynamic title

--- src/app/post/[id]/page.js
+++ src/app/post/[id]/page.js
@@ -1,11 +1,22 @@
 import getPost from '@/libs/getPost';
 import React from 'react'
 
+export async function generateMetadata({params}) {
+  const {id} = params;
+  const postDetails = await getPost(id);
+
+  return{
+    title: postDetails.title,
+    description : postDetails.body,
+  }      
+}
+
+
 export default async function PostPage({params}) {
     const {id} = params;
     const postDetails = await getPost(id);
-    console.log(postDetails);
-    
+    //console.log(postDetails);
+  
   return (
     <div className="m-5">
         <h2 className='text-blue-600'>title : {postDetails.title}</h2>
