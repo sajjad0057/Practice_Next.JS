57a8673f8715c40960040f88ec2393fe12e1c77c
sajjad0057 <sajjadhossain0057@gmail.com>
2024-10-29 14:25:47 +0600

    static metadata

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,7 +1,12 @@
 import React from 'react'
 
+export const metadata = {
+  title : "About Us",
+  description : "This is blog about page"
+}
+
 export default function About() {
-    throw new Error("Fetal error occured!");
+    //throw new Error("Fetal error occured!");
   return (
     <main className="mt-10">
         <div>This is about page!</div>
