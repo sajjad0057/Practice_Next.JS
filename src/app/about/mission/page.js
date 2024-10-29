a9729c49dcaa360e12e8f23842b14097d5f22126
sajjad0057 <sajjadhossain0057@gmail.com>
2024-10-29 14:05:35 +0600

    Using Image Component and potimized image

--- src/app/about/mission/page.js
+++ src/app/about/mission/page.js
@@ -1,11 +1,16 @@
-import Button from '@/app/components/button';
+import Button from '@/app/components/Button';
+import Image from 'next/image';
 import React from 'react'
+import thumb from '@/public/images/one.jpg'
 
 export default function Mission() {
   return (
     <main className="mt-10">
         <div>This is mission page!</div>
         <hr />
+        <div className="w-[400px]">
+          <Image placeholder='blur' src={thumb} alt='node.js image' quality={100}/>
+        </div>
         <Button/>
     </main>
   )
