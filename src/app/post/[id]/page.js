c5a4577932f4c677eb602be91b03495af62fd57a
sajjad0057 <sajjadhossain0057@gmail.com>
2024-10-29 15:49:33 +0600

    make parallel executions to fetch post and postComments

--- src/app/post/[id]/page.js
+++ src/app/post/[id]/page.js
@@ -1,4 +1,5 @@
 import getPost from '@/libs/getPost';
+import getPostComments from '@/libs/getPostComments';
 import React from 'react'
 
 export async function generateMetadata({params}) {
@@ -14,14 +15,25 @@ export async function generateMetadata({params}) {
 
 export default async function PostPage({params}) {
     const {id} = params;
-    const postDetails = await getPost(id);
-    //console.log(postDetails);
+    const postPromise= getPost(id);
+    const commentsPromise = getPostComments(id);
+  
+    const [post, comments] = await Promise.all([postPromise,commentsPromise]);
   
   return (
     <div className="m-5">
-        <h2 className='text-blue-600'>title : {postDetails.title}</h2>
+        <h2 className='text-blue-600'>title : {post.title}</h2>
         <hr />
-        <p className='text-orange-600'>description : {postDetails.body}</p>
+        <p className='text-orange-600'>description : {post.body}</p>
+        <hr /><hr />
+        <div className="mt-5">
+          <h2>Comments:</h2>
+          <ul>
+            {comments.map(c=>(
+              <li className='m-4' key={c.id}>{c.body}</li>
+            ))}
+          </ul>
+        </div>
     </div>
   )
 }
